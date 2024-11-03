1f7abcd65758ab72a1d22a8e1d17cacda18bfcb2
sbhatnagar-unr <sbhatnagar@unr.edu>
2024-11-03 12:24:13 -0800

    Created empty LightEnemy Actor

---
+++ Source/ShatteredChains/Private/LightEnemy.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LightEnemy.h"
+#include "ShatteredChains/Logging.h"
+
+DEFINE_LOG_CATEGORY(Enemy);
+
+// Sets default values
+ALightEnemy::ALightEnemy()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	UE_LOG(Enemy, Log, TEXT("New light enemy created"));
+
+}
+
+// Called when the game starts or when spawned
+void ALightEnemy::BeginPlay()
+{
+	UE_LOG(Enemy, Log, TEXT("Light enemy spawned"));
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALightEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
