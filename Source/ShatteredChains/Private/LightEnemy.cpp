5f36e3e20e16cfb73efd4ec87ad7249120542b75
sbhatnagar-unr <sbhatnagar@unr.edu>
2024-11-03 17:24:36 -0800

    Added free asset found online for testing enemy class (asset will be replaced later)

--- Source/ShatteredChains/Private/LightEnemy.cpp
+++ Source/ShatteredChains/Private/LightEnemy.cpp
@@ -12,7 +12,10 @@ ALightEnemy::ALightEnemy()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	
-	UE_LOG(Enemy, Log, TEXT("New light enemy created"));
+	SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Skeletal Mesh"));
+	RootComponent = SkeletalMesh;
+
+	UE_LOG(Enemy, Log, TEXT("New light enemy created (not yet spawned)"));
 
 }
 
--- Source/ShatteredChains/Private/LightEnemy.h
+++ Source/ShatteredChains/Private/LightEnemy.h
@@ -15,6 +15,9 @@ public:
 	// Sets default values for this actor's properties
 	ALightEnemy();
 
+	UPROPERTY(EditAnywhere)
+	class USkeletalMeshComponent* SkeletalMesh;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
