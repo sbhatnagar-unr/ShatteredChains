7b974b4300e7decbe1d33f1dad8be578a09b1088
sbhatnagar-unr <sbhatnagar@unr.edu>
2024-11-06 12:31:33 -0800

    Added mesh and animation class to LightEnemy

--- Source/ShatteredChains/Private/LightEnemy.h
+++ Source/ShatteredChains/Private/LightEnemy.h
@@ -15,15 +15,19 @@ public:
 	// Sets default values for this actor's properties
 	ALightEnemy();
 
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
 	UPROPERTY(EditAnywhere)
-	class USkeletalMeshComponent* SkeletalMesh;
+	USkeletalMeshComponent* skeletal_mesh_component;
+
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
+private:
+	FString skeletal_mesh_asset_path = FString(TEXT("/Game/Enemies/LightEnemy/motion-dummy_female.motion-dummy_female"));
+	FString animation_blueprint_class_path = FString(TEXT("/Game/Enemies/LightEnemy/ABP_Walking.ABP_Walking_C"));
+	
 };
