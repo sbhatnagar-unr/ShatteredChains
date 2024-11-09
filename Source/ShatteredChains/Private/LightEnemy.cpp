7e2f5844eb3424e4305859d75cfd024846348270
sbhatnagar-unr <sbhatnagar@unr.edu>
2024-11-09 00:51:54 -0800

    Gave LightEnemey an AI Controller as well as a target actor for it to chase

--- Source/ShatteredChains/Private/LightEnemy.cpp
+++ Source/ShatteredChains/Private/LightEnemy.cpp
@@ -3,8 +3,7 @@
 
 #include "LightEnemy.h"
 #include "ShatteredChains/Logging.h"
-
-#include <stdexcept>
+#include "Kismet/GameplayStatics.h"
 
 DEFINE_LOG_CATEGORY(Enemy);
 
@@ -19,6 +18,7 @@ ALightEnemy::ALightEnemy()
 	RootComponent = skeletal_mesh_component;
 
 	// Locate neccesary assets
+	UE_LOG(Enemy, Log, TEXT("Trying to load mesh and animation assets...."));
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh> skeletal_mesh(*skeletal_mesh_asset_path); // Is the star overloaded wtf literally why?!
 	static ConstructorHelpers::FClassFinder<UAnimInstance> animation_class(*animation_blueprint_class_path); // Is the star overloaded wtf literally why?!
 
@@ -37,17 +37,51 @@ ALightEnemy::ALightEnemy()
 	// Set the components
 	skeletal_mesh_component->SetSkeletalMesh(skeletal_mesh.Object);
 	skeletal_mesh_component->SetAnimClass(animation_class.Class);
+	UE_LOG(Enemy, Log, TEXT("Mesh and animation assets loaded successfully"));
 
+	// SUCCESS YAY!!!!
 	UE_LOG(Enemy, Log, TEXT("New light enemy created (not yet spawned)"));
 }
+
 
 // Called when the game starts or when spawned
 void ALightEnemy::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(Enemy, Log, TEXT("Light enemy spawned"));
-	
+
+	bool error_occured = false;
+
+	// Get AI Controller
+	UE_LOG(Enemy, Log, TEXT("Trying to get AI Controller...."));
+	ai_controller = Cast<AAIController>(GetController());
+
+	// Validate AI Controller
+	if (ai_controller == nullptr) {
+		UE_LOG(Enemy, Error, TEXT("Unable to get AI Controller"));
+		error_occured = true;
+	} else {
+		UE_LOG(Enemy, Log, TEXT("AI Controller successfully retrieved"));
+	}
+
+	// Get player actor
+	target_actor = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+	// Validate player actor
+	if (target_actor == nullptr) {
+		UE_LOG(Enemy, Error, TEXT("Unable to get target actor (player)"));
+		error_occured = true;
+	} else {
+		UE_LOG(Enemy, Log, TEXT("Target actor successfully retrieved (player)"));
+	}
+
+	// Adjust log level if an error occured
+	if (error_occured) {
+		UE_LOG(Enemy, Warning, TEXT("Light enemy spawned"));
+	} else {
+		UE_LOG(Enemy, Log, TEXT("Light enemy spawned"));
+	}
 }
+
 
 // Called every frame
 void ALightEnemy::Tick(float DeltaTime)
@@ -56,3 +90,8 @@ void ALightEnemy::Tick(float DeltaTime)
 
 }
 
+
+void ALightEnemy::go_to_location(const FVector &location)
+{
+
+}
