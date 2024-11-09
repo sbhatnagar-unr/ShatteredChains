a7f208738f799b6966042dde20033509ba217423
sbhatnagar-unr <sbhatnagar@unr.edu>
2024-11-09 00:51:54 -0800

    Gave LightEnemey an AI Controller as well as a target actor for it to chase

--- Source/ShatteredChains/Private/LightEnemy.h
+++ Source/ShatteredChains/Private/LightEnemy.h
@@ -2,12 +2,14 @@
 
 #pragma once
 
+#include "AIController.h"
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+
 #include "LightEnemy.generated.h"
 
+
 UCLASS()
-class ALightEnemy : public AActor
+class ALightEnemy : public APawn
 {
 	GENERATED_BODY()
 	
@@ -18,15 +20,20 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	USkeletalMeshComponent* skeletal_mesh_component;
-
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 private:
+	// Root component
+	USkeletalMeshComponent* skeletal_mesh_component;
+
+	// Asset paths
 	FString skeletal_mesh_asset_path = FString(TEXT("/Game/Enemies/LightEnemy/motion-dummy_female.motion-dummy_female"));
 	FString animation_blueprint_class_path = FString(TEXT("/Game/Enemies/LightEnemy/ABP_Walking.ABP_Walking_C"));
-	
+
+	// Player tracking
+	AAIController* ai_controller;
+	AActor* target_actor;
+	void go_to_location(const FVector &location);
 };
