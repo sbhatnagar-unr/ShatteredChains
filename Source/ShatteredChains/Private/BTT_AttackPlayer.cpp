942243b546259439ab8d59cdd4318510dbaaeb30
sbhatnagar-unr <sbhatnagar@unr.edu>
2024-11-20 15:33:55 -0800

    Refactored attacking BTTask

--- Source/ShatteredChains/Private/BTT_AttackPlayer.cpp
+++ Source/ShatteredChains/Private/BTT_AttackPlayer.cpp
@@ -7,6 +7,7 @@
 #include "AIController.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "Kismet/KismetMathLibrary.h"
+#include "ShatteredChains/Utility.h"
 
 
 UBTT_AttackPlayer::UBTT_AttackPlayer()
@@ -17,69 +18,35 @@ UBTT_AttackPlayer::UBTT_AttackPlayer()
 
 EBTNodeResult::Type UBTT_AttackPlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	// Get AI Controller
-	AAIController* ai_controller = OwnerComp.GetAIOwner();
-	// Make sure AI Controller is valid
-	if (ai_controller == nullptr)
-	{
-		UE_LOG(Enemy, Warning, TEXT("BTT_AttackPlayer enemy AI could not get AI Controller"));
-		// In here, blackboard in_attacking_range is already false, or else this function wouldnt have ran to begin with
-		return EBTNodeResult::Type::Failed;
-	}
-
-	// Get the blackboard
-	UBlackboardComponent* blackboard = ai_controller->GetBlackboardComponent();
-	// Make sure the blackboard is valid
-	if (blackboard == nullptr)
-	{
-		UE_LOG(Enemy, Error, TEXT("BTT_AttackPlayer could not get Light Enemy AI blackboard"));
-		// In here, blackboard in_attacking_range is already false, or else this function wouldnt have ran to begin with
-		return EBTNodeResult::Type::Failed;
-	}
-
-	// Field names that dont change
 	static const FName in_attacking_range_field(TEXT("in_attacking_range"));
+	static FDateTime last_animation_end_time = FDateTime::MinValue();
 
-
-	// The enemy that this AI belongs to
-	ALightEnemy* enemy_actor = (ALightEnemy*) ai_controller->GetPawn();
-	// Make sure the enemy_actor is valid
-	if (enemy_actor == nullptr)
+	// Enemy AI Controller
+	AAIController* ai_controller;
+	// Enemy AI Blackboard
+	UBlackboardComponent* blackboard;
+	// Enemy Actor
+	ALightEnemy* enemy_actor;
+	// Enemy's target actor
+	AActor* target_actor;
+	// Enemy's animation instance
+	UAnimInstance* anim_instance;
+	// Enemy's attack animation montage
+	UAnimMontage* attack_animation_montage;
+	try
 	{
-		UE_LOG(Enemy, Error, TEXT("BTT_AttackPlayer could not get enemy actor belonging to this AI"));
-		// In here, blackboard in_attacking_range is already false, or else this function wouldnt have ran to begin with
+		ai_controller = Validity::check_value<AAIController>(OwnerComp.GetAIOwner(), "Could not get AI Controller");
+		blackboard = Validity::check_value<UBlackboardComponent>(ai_controller->GetBlackboardComponent(), "Could not get Light Enemy AI blackboard");
+		enemy_actor = Validity::check_value<ALightEnemy>((ALightEnemy*)ai_controller->GetPawn(), "Could not get enemy actor belonging to this AI");
+		target_actor = Validity::check_value<AActor>(enemy_actor->get_target(), "Enemy AI could not get player actor");
+		anim_instance = Validity::check_value<UAnimInstance>(enemy_actor->GetMesh()->GetAnimInstance(), "No anim instance for Light Enemy");
+		attack_animation_montage = Validity::check_value<UAnimMontage>(enemy_actor->get_attack_animation_montage(), "No animation montage for Light Enemy attack");
+	}
+	catch (const Validity::NullPointerException& e)
+	{
+		UE_LOG(Enemy, Error, LOG_TEXT("%hs"), e.what());
 		return EBTNodeResult::Type::Failed;
 	}
-
-	// The target to chase
-	AActor* target_actor = enemy_actor->get_target();
-
-	// Check if there is a target
-	if (target_actor == nullptr)
-	{
-		UE_LOG(Enemy, Warning, TEXT("BTT_AttackPlayer enemy AI could not get player actor"));
-		// In here, blackboard in_attacking_range is already false, or else this function wouldnt have ran to begin with
-		return EBTNodeResult::Type::Failed;
-	}
-
-	// Get the anim instance
-	UAnimInstance* anim_instance = enemy_actor->GetMesh()->GetAnimInstance();
-	if (anim_instance == nullptr)
-	{
-		UE_LOG(Enemy, Warning, TEXT("BTT_AttackPlayer no anim instance for Light Enemy"));
-		// return early
-		return EBTNodeResult::Type::Failed;
-	}
-
-	// Get the attack animation
-	UAnimMontage* attack_animation_montage = enemy_actor->get_attack_animation_montage();
-	if (attack_animation_montage == nullptr)
-	{
-		UE_LOG(Enemy, Warning, TEXT("BTT_AttackPlayer no animation montage for Light Enemy attack"));
-		// return early
-		return EBTNodeResult::Type::Failed;
-	}
-
 
 	// Get locations of enemy and target
 	FVector enemy_location = enemy_actor->GetActorLocation();
@@ -90,42 +57,54 @@ EBTNodeResult::Type UBTT_AttackPlayer::ExecuteTask(UBehaviorTreeComponent& Owner
 
 	// If we are withing attacking range
 	float attack_range = enemy_actor->get_attack_range();
-	UE_LOG(Enemy, VeryVerbose, TEXT("BTT_AttackPlayer enemy distance to target %f (attack range: %f)"), distance, attack_range);
+	UE_LOG(Enemy, VeryVerbose, LOG_TEXT("Enemy distance to target %f (attack range: %f)"), distance, attack_range);
 
 	if (distance <= attack_range)
 	{
-		UE_LOG(Enemy, Log, TEXT("BTT_AttackPlayer enemy is attacking"));	
+		UE_LOG(Enemy, Verbose, LOG_TEXT("Enemy is attacking"));
 
 		// Rotate to face the player
 		FRotator rotation_to_player = UKismetMathLibrary::FindLookAtRotation(enemy_location, target_location);
 		enemy_actor->SetActorRotation(rotation_to_player);
-		UE_LOG(Enemy, Verbose, TEXT("BTT_AttackPlayer rotating to look at player"));
+		UE_LOG(Enemy, Verbose, LOG_TEXT("Rotating to look at player"));
 
-		// Play the animation
-		float duration = anim_instance->Montage_Play(attack_animation_montage, 1.0f, EMontagePlayReturnType::MontageLength, 0.0f, true);
-
-		// If duration == 0.f that means an error
-		if (duration == 0.f)
+		// Check if current time is after the last animtion stop time
+		if (FDateTime::Now() > last_animation_end_time)
 		{
-			UE_LOG(Enemy, Error, TEXT("BTT_AttackPlayer Light Enemy could not play attack animation montage"));
-			// return early
-			return EBTNodeResult::Type::Failed;
-		}	
+			// If the last animation has ended, we can start a new one and put a new end time
+			// Play the animation and get its duration in seconds
+			UE_LOG(Enemy, Verbose, LOG_TEXT("Starting new attack animation montage"));
+			float duration = anim_instance->Montage_Play(attack_animation_montage, 1.0f, EMontagePlayReturnType::MontageLength, 0.0f, true);
+
+			// Update the end time
+			last_animation_end_time = FDateTime::Now() + FTimespan::FromSeconds(duration);
+			// If duration == 0.f that means an error
+			if (duration == 0.f)
+			{
+				UE_LOG(Enemy, Error, LOG_TEXT("Could not play attack animation montage"));
+				// return early
+				return EBTNodeResult::Type::Failed;
+			}
+		}
 	}
 	// Otherwise
 	else
 	{
+		UE_LOG(Enemy, Verbose, TEXT("Target is out of range"));
+
 		// Stop the montage
 		if (anim_instance->Montage_IsPlaying(attack_animation_montage))
 		{
 			anim_instance->Montage_Stop(enemy_actor->get_attack_animation_exit_blend(), attack_animation_montage);
-			UE_LOG(Enemy, Verbose, TEXT("BTT_AttackPlayer stopping montage"));
+			UE_LOG(Enemy, VeryVerbose, LOG_TEXT("Stopping montage"));
+			// Set the end time so next time this gets called it can start instantly
+			last_animation_end_time = FDateTime::Now();
 		}
 
 		// Go back to chase player
-		UE_LOG(Enemy, Log, TEXT("BTT_AttackPlayer target is out of range"));
 		blackboard->SetValueAsBool(in_attacking_range_field, false);
-
 	}
+
+	// Succsess
 	return EBTNodeResult::Type::Succeeded;
 }
