7a709547686e6b281fef6a3173c11e72e55a3f42
sbhatnagar-unr <sbhatnagar@unr.edu>
2024-11-19 17:50:44 -0800

    Created rudimentary health system

--- Source/ShatteredChains/Private/CharacterWithHealth.cpp
+++ Source/ShatteredChains/Private/CharacterWithHealth.cpp
@@ -2,7 +2,10 @@
 
 
 #include "CharacterWithHealth.h"
+#include "ShatteredChains/Logging.h"
 
+ DEFINE_LOG_CATEGORY(Health);
+ 
 
 float ACharacterWithHealth::get_health() const
 {
@@ -18,20 +21,37 @@ float ACharacterWithHealth::get_max_health() const
 
 void ACharacterWithHealth::heal(float health)
 {
+	/*
+	Heals charachter up to a maxiumim of max_health
+	*/
 	current_health += health;
 	if (current_health > max_health)
 	{
+		UE_LOG(Health, Log, TEXT("Overhealed, resetting health to max"));
+		current_health = max_health;
 	}
 }
 
 
 void ACharacterWithHealth::set_health(float health)
 {
-
+	/*
+	Sets health up to a maximum of max_health
+	*/
+	current_health = (health > max_health) ? max_health : health;
 }
 
 
 void ACharacterWithHealth::deal_damage(float damage)
 {
+	/*
+	Applies damage, if health becomes negative, it gets set to 0
+	*/
+	current_health -= damage;
 
+	if (current_health <= 0)
+	{
+		current_health = 0;
+		dead();
+	}
 }
