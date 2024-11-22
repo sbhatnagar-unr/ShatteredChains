1760673fa4f5c91b03b105b67c95cbad2c68a5f9
sbhatnagar-unr <sbhatnagar@unr.edu>
2024-11-22 00:56:06 -0800

    Created weapon base class

---
+++ Source/ShatteredChains/Private/Weapons/Guns/Weapon.cpp
@@ -0,0 +1,146 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon.h"
+#include "ShatteredChains/Logging.h"
+#include "ShatteredChains/Utility.h"
+
+DEFINE_LOG_CATEGORY(Weapons);
+
+// Sets default values
+AWeapon::AWeapon()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	magazine_size = 0;
+	current_mag_ammo_count = 0;
+	max_ammo_stock_pile_count = 0;
+	current_ammo_stock_pile_count = 0;
+	weapon_skeletal_mesh_component = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Weapon Skeletal Mesh"));
+	RootComponent = weapon_skeletal_mesh_component;
+}
+
+// Called when the game starts or when spawned
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+
+	try
+	{
+		// Check for mesh
+		Validity::check_value<USkeletalMesh>(weapon_skeletal_mesh_component->GetSkeletalMeshAsset(), "No skeletal mesh for weapon");
+		UE_LOG(Weapons, Verbose, LOG_TEXT("Weapon has skeletal mesh"));
+
+		// Check for animations
+		Validity::check_value<UAnimMontage>(fire_animation_montage, "No fire animation montage for weapon");
+		Validity::check_value<UAnimMontage>(reload_animation_montage, "No reload animation montage for weapon");
+		UE_LOG(Weapons, Verbose, LOG_TEXT("Weapon has animation montages"));
+	}
+	catch (const Validity::NullPointerException& e)
+	{
+		UE_LOG(Weapons, Error, LOG_TEXT("%hs"), e.what());
+	}
+
+	// Set ammo
+	current_ammo_stock_pile_count = max_ammo_stock_pile_count;
+	refill_magazine();
+}
+
+
+void AWeapon::fire() const
+{
+	// Weapons Animation instance
+	UAnimInstance* anim_instance;
+	try
+	{
+		Validity::check_value<UAnimMontage>(fire_animation_montage, "No fire animation montage for weapon");
+		anim_instance = Validity::check_value<UAnimInstance>(this->weapon_skeletal_mesh_component->GetAnimInstance(), "No anim instance found");
+	}
+	catch (const Validity::NullPointerException& e)
+	{
+		UE_LOG(Weapons, Error, LOG_TEXT("%hs"), e.what());
+		return;
+	}
+
+	UE_LOG(Weapons, Verbose, LOG_TEXT("Playing weapon fire animation montage"));
+	float duration = anim_instance->Montage_Play(fire_animation_montage, 1.0f, EMontagePlayReturnType::MontageLength, 0.0f, true);
+
+	// If duration == 0.f that means an error
+	if (duration == 0.f)
+	{
+		UE_LOG(Weapons, Error, LOG_TEXT("Could not play weapon fire animation montage"));
+		return;
+	}
+}
+
+
+void AWeapon::reload() const
+{
+	// Weapons Animation instance
+	UAnimInstance* anim_instance;
+	try
+	{
+		Validity::check_value<UAnimMontage>(reload_animation_montage, "No reload animation montage for weapon");
+		anim_instance = Validity::check_value<UAnimInstance>(this->weapon_skeletal_mesh_component->GetAnimInstance(), "No anim instance found");
+	}
+	catch (const Validity::NullPointerException& e)
+	{
+		UE_LOG(Weapons, Error, LOG_TEXT("%hs"), e.what());
+		return;
+	}
+
+	UE_LOG(Weapons, Verbose, LOG_TEXT("Playing weapon reload animation montage"));
+	float duration = anim_instance->Montage_Play(reload_animation_montage, 1.0f, EMontagePlayReturnType::MontageLength, 0.0f, true);
+
+	// If duration == 0.f that means an error
+	if (duration == 0.f)
+	{
+		UE_LOG(Weapons, Error, LOG_TEXT("Could not play weapon reload animation montage"));
+		return;
+	}
+}
+
+
+unsigned int AWeapon::get_magazine_size() const
+{
+	return magazine_size;
+}
+
+
+unsigned int AWeapon::get_current_mag_ammo_count() const
+{
+	return current_mag_ammo_count;
+}
+
+
+unsigned int AWeapon::get_max_ammo_stock_pile_count() const
+{
+	return max_ammo_stock_pile_count;
+}
+
+
+unsigned int AWeapon::get_current_ammo_stock_pile_count() const
+{
+	return current_ammo_stock_pile_count;
+}
+
+
+void AWeapon::refill_magazine()
+{
+	unsigned int remaining_mag_space = magazine_size - current_mag_ammo_count;
+
+	// If we have enough ammo to top it off
+	if (current_ammo_stock_pile_count >= remaining_mag_space)
+	{
+		UE_LOG(Weapons, Verbose, LOG_TEXT("Topping off weapon magazine"));
+		current_ammo_stock_pile_count -= remaining_mag_space;
+		current_mag_ammo_count = magazine_size;
+	}
+	// Otherwise
+	else
+	{
+		UE_LOG(Weapons, Verbose, LOG_TEXT("Putting last ammo in weapon magazine"));
+		current_mag_ammo_count += current_ammo_stock_pile_count;
+		current_ammo_stock_pile_count = 0;
+	}
+}
---
+++ Source/ShatteredChains/Private/Weapons/Guns/Weapon.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UCLASS()
+class AWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AWeapon();
+
+	void fire() const;
+	void reload() const;
+
+	unsigned int get_magazine_size() const;
+	unsigned int get_current_mag_ammo_count() const;
+
+	unsigned int get_max_ammo_stock_pile_count() const;
+	unsigned int get_current_ammo_stock_pile_count() const;
+
+	virtual void refill_magazine();
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<USkeletalMeshComponent> weapon_skeletal_mesh_component;
+
+	UPROPERTY(EditDefaultsOnly, Category="Ammo")
+	unsigned int magazine_size;
+
+	UPROPERTY(EditDefaultsOnly, Category="Ammo")
+	unsigned int max_ammo_stock_pile_count;
+
+	unsigned int current_mag_ammo_count;
+	unsigned int current_ammo_stock_pile_count;
+
+	// Animation montages
+	UPROPERTY(EditDefaultsOnly, Category = "Animations")
+	TObjectPtr<UAnimMontage> fire_animation_montage;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Animations")
+	TObjectPtr<UAnimMontage> reload_animation_montage;
+};
