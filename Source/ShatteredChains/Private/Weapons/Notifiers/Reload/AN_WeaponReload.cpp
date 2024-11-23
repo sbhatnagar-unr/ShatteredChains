392f0207be2abe61dfe375d0f703a964636299b7
sbhatnagar-unr <sbhatnagar@unr.edu>
2024-11-22 22:14:00 -0800

    Added reload notifier functionality

--- Source/ShatteredChains/Private/Weapons/Notifiers/Reload/AN_WeaponReload.cpp
+++ Source/ShatteredChains/Private/Weapons/Notifiers/Reload/AN_WeaponReload.cpp
@@ -2,4 +2,33 @@
 
 
 #include "AN_WeaponReload.h"
+#include "ShatteredChains/Utility.h"
+#include "ShatteredChains/Logging.h"
+#include "Camera/CameraComponent.h"
+#include "Engine/HitResult.h"
+#include "CollisionQueryParams.h"
+#include "../../../Characters/Player/MyCharacter.h"
+#include "../../Guns/Weapon.h"
+
+
+void UAN_WeaponReload::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	AWeapon* weapon;
+
+	try
+	{
+		MeshComp = Validity::check_value<USkeletalMeshComponent>(MeshComp, "No mesh for animation notify");
+		weapon = Validity::check_value<AWeapon>(Cast<AWeapon>(MeshComp->GetOwner()), "Could not get weapon actor from MeshComp");
+	}
+	catch (const Validity::NullPointerException& e)
+	{
+		UE_LOG(Weapons, Error, LOG_TEXT("%hs"), e.what());
+		return;
+	}
+
+	// Reload the weapon
+	weapon->refill_magazine();
+	UE_LOG(Weapons, Log, LOG_TEXT("Weapon reloaded"));
+
+}
 
--- Source/ShatteredChains/Private/Weapons/Notifiers/Reload/AN_WeaponReload.h
+++ Source/ShatteredChains/Private/Weapons/Notifiers/Reload/AN_WeaponReload.h
@@ -14,4 +14,6 @@ class UAN_WeaponReload : public UAnimNotify
 {
 	GENERATED_BODY()
 	
+	void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
 };
