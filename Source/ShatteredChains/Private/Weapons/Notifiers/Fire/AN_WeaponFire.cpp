08db25f7f357a70174e1fe58a1e836e480a7dd30
sbhatnagar-unr <sbhatnagar@unr.edu>
2024-11-23 19:47:03 -0800

    Working hitscan logic, but its not attached to weapon holder yet

--- Source/ShatteredChains/Private/Weapons/Notifiers/Fire/AN_WeaponFire.cpp
+++ Source/ShatteredChains/Private/Weapons/Notifiers/Fire/AN_WeaponFire.cpp
@@ -2,46 +2,101 @@
 
 
 #include "AN_WeaponFire.h"
+#include "Camera/CameraComponent.h"
+#include "../../../Characters/Player/MyCharacter.h"
+#include "../../Weapon.h"
+#include "../../../Characters/CharacterWithHealth.h"
 #include "ShatteredChains/Logging.h"
-
+#include "ShatteredChains/Utility.h"
 
 void UAN_WeaponFire::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
 {
 
 	// The players camera
-	//UCameraComponent* camera;
-	//AWeapon* weapon;
-	//
-	//try
-	//{
-	//	MeshComp = Validity::check_value<USkeletalMeshComponent>(MeshComp, "No mesh for animation notify");
-	//	weapon = Validity::check_value<AWeapon>(Cast<AWeapon>(MeshComp->GetOwner()), "Could not get weapon actor from MeshComp");
-	//	AMyCharacter* weapon_holder = Validity::check_value<AMyCharacter>(Cast<AMyCharacter>(weapon->GetAttachParentActor()), "Could not get weapon holder from weapon");
-	//	camera = Validity::check_value<UCameraComponent>(weapon_holder->Camera, "Coul not get camera component from character");
-	//}
-	//catch (const Validity::NullPointerException& e)
-	//{
-	//	UE_LOG(Weapons, Error, LOG_TEXT("%hs"), e.what());
-	//	return;
-	//}
-	//
-	//// World is needed for the scan
-	//UWorld* world = GetWorld();
-	//
-	//// Starting position
-	//FVector trace_start = camera->GetComponentLocation();
-	//// End position
-	//FVector trace_end = camera->GetForwardVector() * weapon->get_scan_distance();
-	//
-	//// Trace results
-	//struct FHitResult trace_result;
-	//
-	//struct FCollisionQueryParams query_params;
-	//// Actually check if it hits the mesh
-	//query_params.bTraceComplex = true;
-	////world->LineTraceSingleByChannel(trace_result, trace_start, trace_end, ECollisionChannel::ECC_Visibility, );
-	//
-	//DrawDebugLine();
+	UCameraComponent* camera;
+	// The weapon
+	AWeapon* weapon;
+	// The charachter holding the weapon
+	AMyCharacter* weapon_holder;
+	// World is needed for the scan
+	UWorld* world;
+	try
+	{
+		MeshComp = Validity::check_value<USkeletalMeshComponent>(MeshComp, "No mesh for animation notify");
+		weapon = Validity::check_value<AWeapon>(Cast<AWeapon>(MeshComp->GetOwner()), "Could not get weapon actor from MeshComp");
+		//weapon_holder = Validity::check_value<AMyCharacter>(Cast<AMyCharacter>(weapon->GetAttachParentActor()), "Could not get weapon holder from weapon");
+		//camera = Validity::check_value<UCameraComponent>(weapon_holder->Camera, "Coul not get camera component from character");
+		world = Validity::check_value<UWorld>(MeshComp->GetWorld(), "Could not get world");
+	}
+	catch (const Validity::NullPointerException& e)
+	{
+		UE_LOG(Weapons, Error, LOG_TEXT("%hs"), e.what());
+		return;
+	}
 
+	/*TEST CODE*/
+	/*TEST CODE*/
+	/*TEST CODE*/
+	/*TEST CODE*/
+	AMyCharacter* player = Cast<AMyCharacter>(world->GetFirstPlayerController()->GetCharacter());
+	if (player == nullptr)
+	{
+		UE_LOG(Weapons, Error, LOG_TEXT("CANT GET PLAYER"));
+		return;
+	}
+	camera = player->Camera;
+	weapon_holder = nullptr;
+	/*DELETE AFTER TESTING*/
+	/*DELETE AFTER TESTING*/
+	/*DELETE AFTER TESTING*/
+	/*DELETE AFTER TESTING*/
+
+
+	// Trace results
+	struct FHitResult trace_result;
+	// Starting position
+	FVector trace_start = camera->GetComponentLocation();
+	// End position
+	FVector trace_end = trace_start + (camera->GetForwardVector() * weapon->get_scan_distance());
+	// Trace params
+	struct FCollisionQueryParams trace_params;
+	// Actually check if it hits the mesh
+	trace_params.bTraceComplex = true;
+
+
+	// Perform the trace
 	UE_LOG(Weapons, Log, LOG_TEXT("Firing weapon"));
+	UE_LOG(Weapons, VeryVerbose, LOG_TEXT("[Line Trace] %s -> %s"), *trace_start.ToString(), *trace_end.ToString());
+	bool hit_something = world->LineTraceSingleByChannel(trace_result, trace_start, trace_end, ECollisionChannel::ECC_Visibility, trace_params);
+	
+	// Draw a debug line
+	DrawDebugLine(world, trace_start, trace_end, FColor::Red, false, 0.07f, 0U, 0.3f);
+
+	if (hit_something)
+	{
+		// Draw box around the spot we hit
+		DrawDebugBox(world, trace_result.ImpactPoint, FVector(1, 1, 1), FColor::Blue, false, 3.f, 0U, 5.f);
+		// Check if the actor was a charachter with health
+		ACharacterWithHealth* hit_actor;
+		try
+		{
+			hit_actor = Validity::check_value<ACharacterWithHealth>(Cast<ACharacterWithHealth>(trace_result.GetActor()), "");
+		}
+		catch (const Validity::NullPointerException&)
+		{
+			// Log the thing we hits name
+			UE_LOG(Weapons, Verbose, LOG_TEXT("Trace hit undamageable target (%s)"), *(trace_result.GetActor()->GetName()));
+			return;
+		}
+	
+		// If the hit actor has health
+		float weapon_damage = weapon->get_weapon_damage();
+		UE_LOG(Weapons, Log, LOG_TEXT("Trace hit target (%s), %f damage dealt"), *(trace_result.GetActor()->GetName()), weapon_damage);
+		hit_actor->deal_damage(weapon_holder, weapon_damage);
+	}
+	else
+	{
+		UE_LOG(Weapons, Verbose, LOG_TEXT("Trace hit no target"));
+	}
+
 }
