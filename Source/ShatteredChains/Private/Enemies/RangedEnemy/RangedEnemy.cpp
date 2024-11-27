bc47762a7910dbbd9e8913b19c51deb71c4d556a
sbhatnagar-unr <sbhatnagar@unr.edu>
2024-11-27 00:08:19 -0800

    Removed canActorTick=True from constructor of ranged enemy, letting it inherit from parent class Enemy

--- Source/ShatteredChains/Private/Enemies/RangedEnemy/RangedEnemy.cpp
+++ Source/ShatteredChains/Private/Enemies/RangedEnemy/RangedEnemy.cpp
@@ -8,7 +8,6 @@
 ARangedEnemy::ARangedEnemy() : AEnemy()
 {
     // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-    PrimaryActorTick.bCanEverTick = true;
 }
 
 // Called when the game starts or when spawned
