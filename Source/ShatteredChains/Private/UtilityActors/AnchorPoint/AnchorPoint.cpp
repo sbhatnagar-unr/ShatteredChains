f236da993eeb61880b7a410a6f7da2da16b42380
sbhatnagar-unr <sbhatnagar@unr.edu>
2024-12-05 01:41:01 -0800

    changed default anchor radius from 50 to 200

--- Source/ShatteredChains/Private/UtilityActors/AnchorPoint/AnchorPoint.cpp
+++ Source/ShatteredChains/Private/UtilityActors/AnchorPoint/AnchorPoint.cpp
@@ -14,7 +14,7 @@ AAnchorPoint::AAnchorPoint()
 {
     // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
     PrimaryActorTick.bCanEverTick = false;
-    anchor_radius = 50;
+    anchor_radius = 200;
     mesh_component = nullptr;
     sphere_component = nullptr;
     
