1d6c59312227844c8cf6348471de610f8b41518b
sbhatnagar-unr <sbhatnagar@unr.edu>
2025-01-29 13:36:13 -0800

    UPD: Added HasInventory interface

---
+++ Source/ShatteredChains/Private/Interfaces/HasInventory/HasInventory.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HasInventory.h"
+
+
+// Add default functionality here for any IHasInventory functions that are not pure virtual.
