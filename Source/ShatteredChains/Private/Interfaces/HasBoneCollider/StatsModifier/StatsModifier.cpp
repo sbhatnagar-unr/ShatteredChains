c130b54093e0b6c02bcfd93e88805e14226728cb
sbhatnagar-unr <sbhatnagar@unr.edu>
2025-02-05 13:42:42 -0800

    UPD: Changed attributes and method names to be more reflective of their functionality

--- Source/ShatteredChains/Private/Interfaces/HasBoneCollider/StatsModifier/StatsModifier.cpp
+++ Source/ShatteredChains/Private/Interfaces/HasBoneCollider/StatsModifier/StatsModifier.cpp
@@ -2,34 +2,35 @@
 
 
 #include "StatsModifier.h"
+
 UStatsModifier::UStatsModifier()
 {
     // Default to values that won't modify anything
-    additive_health_modifier = 0;
-    multiplicative_health_modifier = 1;
+    additive_damage_modifier = 0;
+    multiplicative_damage_modifier = 1;
 }
 
 
 
-float UStatsModifier::get_additive_health_modifier() const
+float UStatsModifier::get_additive_damage_modifier() const
 {
-    return additive_health_modifier;
+    return additive_damage_modifier;
 }
 
 
-float UStatsModifier::get_multiplicative_health_modifier() const
+float UStatsModifier::get_multiplicative_damage_modifier() const
 {
-    return multiplicative_health_modifier;
+    return multiplicative_damage_modifier;
 }
 
 
-void UStatsModifier::set_additive_health_modifier(const float new_additive_health_modifier)
+void UStatsModifier::set_additive_damage_modifier(const float new_additive_damage_modifier)
 {
-    additive_health_modifier = new_additive_health_modifier;
+    additive_damage_modifier = new_additive_damage_modifier;
 }
 
 
-void UStatsModifier::set_multiplicative_health_modifier(const float new_multiplicative_health_modifier)
+void UStatsModifier::set_multiplicative_damage_modifier(const float new_multiplicative_damage_modifier)
 {
-    multiplicative_health_modifier = new_multiplicative_health_modifier;
+    multiplicative_damage_modifier = new_multiplicative_damage_modifier;
 }
--- Source/ShatteredChains/Private/Interfaces/HasBoneCollider/StatsModifier/StatsModifier.h
+++ Source/ShatteredChains/Private/Interfaces/HasBoneCollider/StatsModifier/StatsModifier.h
@@ -16,16 +16,16 @@ class SHATTEREDCHAINS_API UStatsModifier : public UObject
 public:
     UStatsModifier();
 
-    float get_additive_health_modifier() const;
-    float get_multiplicative_health_modifier() const;
+    float get_additive_damage_modifier() const;
+    float get_multiplicative_damage_modifier() const;
 
-    void set_additive_health_modifier(const float new_additive_health_modifier);
-    void set_multiplicative_health_modifier(const float new_multiplicative_health_modifier);
+    void set_additive_damage_modifier(const float new_additive_damage_modifier);
+    void set_multiplicative_damage_modifier(const float new_multiplicative_damage_modifier);
 
 private:
     // These are applied to the base damage
     // Additive is applied first
     // Then multiplicative is applied to the result
-    float additive_health_modifier;
-    float multiplicative_health_modifier;
+    float additive_damage_modifier;
+    float multiplicative_damage_modifier;
 };
