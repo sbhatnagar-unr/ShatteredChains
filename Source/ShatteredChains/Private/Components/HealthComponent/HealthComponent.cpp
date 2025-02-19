8a1992db87da0596b6e3c7b3e84ba5bbc19e973e
sbhatnagar-unr <sbhatnagar@unr.edu>
2025-02-19 11:48:27 -0800

    UPD: Set default health to 100

--- Source/ShatteredChains/Private/Components/HealthComponent/HealthComponent.cpp
+++ Source/ShatteredChains/Private/Components/HealthComponent/HealthComponent.cpp
@@ -14,7 +14,7 @@ UHealthComponent::UHealthComponent()
     // off to improve performance if you don't need them.
     PrimaryComponentTick.bCanEverTick = false;
 
-    max_health = 0.f;
+    max_health = 100.f;
     current_health = 0.f;
     damage_multiplier = 1.f;
     is_dead = false;
