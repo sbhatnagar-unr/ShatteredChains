17e1c8ad65e8fe4674b15d10b2ec9f01139bc16a
sbhatnagar-unr <sbhatnagar@unr.edu>
2025-02-27 20:51:42 -0800

    UPD: Added automated test for Pistol weapon (currently empty but can be used for specific weapon testing later)

---
+++ Source/ShatteredChains/Private/AutomatedTesting/FunctionalTests/Weapons/Pistol/PistolBasicTest.cpp
@@ -0,0 +1,10 @@
+#include "Misc/AutomationTest.h"
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(PistolBasicTest, "ShatteredChains.ShatteredChains.Private.AutomatedTesting.FunctionalTests.Weapons.Pistol.PistolBasicTest",
+                                 EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool PistolBasicTest::RunTest(const FString& Parameters)
+{
+    // Make the test pass by returning true, or fail by returning false.
+    return true;
+}
